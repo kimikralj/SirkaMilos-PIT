49bace910761a9de3ddf751ccb0a1e3f9a4c19cd
kimikralj <74720186+kimikralj@users.noreply.github.com>
2023-12-19 18:59:03 +0100

    Add files via upload

---
+++ WebApplication1/izvestaj.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class izvestaj : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.PreviousPage != null)
+            {
+                lblImeiPrezime.Text = PreviousPage.ImeiPrezime;
+                lblEmail.Text = "(" + PreviousPage.Email + "), rodjen ";
+                lblGodine.Text = PreviousPage.Godine + " godine, učenik ";
+                lblRazred.Text = PreviousPage.Razred + " razreda, uspešno je popunio obrazac za prijavu...";
+            }
+        }
+    }
+}
